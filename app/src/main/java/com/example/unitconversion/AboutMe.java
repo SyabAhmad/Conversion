9ed54da211887a8a51ca7bab45b940cd3b86fe75
DEVELoper <syabblogger@gmail.com>
2023-01-11 17:12:29 +0500

    finalizing app version 2

--- app/src/main/java/com/example/unitconversion/AboutMe.java
+++ app/src/main/java/com/example/unitconversion/AboutMe.java
@@ -2,7 +2,11 @@ package com.example.unitconversion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class AboutMe extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class AboutMe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_me);
+
+
+        ImageButton backButton = (ImageButton) findViewById(R.id.backToMainManu1);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(AboutMe.this, "Home", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(AboutMe.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
