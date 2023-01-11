b63fd1198c8bc6c6a2b2dfe23fc7fe937e21ada8
DEVELoper <syabblogger@gmail.com>
2023-01-11 21:51:22 +0500

    finalizing app version 3 change in about me and adding projects and change in setting

--- app/src/main/java/com/example/unitconversion/setting.java
+++ app/src/main/java/com/example/unitconversion/setting.java
@@ -3,8 +3,10 @@ package com.example.unitconversion;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Toast;
 
@@ -24,6 +26,95 @@ public class setting extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        Button facebook = (Button) findViewById(R.id.openFacebook);
+        facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("www.facebook.com");
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+
+
+        Button telegram = (Button) findViewById(R.id.openTelegram);
+        telegram.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("www.telegram.com");
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+        Button twitter = (Button) findViewById(R.id.openTwitter);
+        twitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("www.twitter.com");
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+        Button linkedIn = (Button) findViewById(R.id.openLinkedIn);
+        linkedIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("www.linkedin.com");
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+        Button itNetwork = (Button) findViewById(R.id.openItNetwork);
+        itNetwork.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("www.itnetwork.com");
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+        Button changeTheme = (Button) findViewById(R.id.changeTheme);
+        changeTheme.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("www.changetheme.com");
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+        Button changeAddress = (Button) findViewById(R.id.changeAddress);
+        changeAddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("www.gmail.com");
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+        Button customerServices = (Button) findViewById(R.id.customerServices);
+        customerServices.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("www.customerservices.com");
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+        Button changeLocation = (Button) findViewById(R.id.changeLocation);
+        changeLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("www.map.com");
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+
+
+
+
 
 
     }
